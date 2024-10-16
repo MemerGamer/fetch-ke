edc31b18ee4add0773244ab401031d37406bcad0
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-10-16 20:05:21 +0300

    feat: working on termux support

--- install.sh
+++ install.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
 # Download the fetch-ke script
-curl -L https://raw.githubusercontent.com/MemerGamer/fetch-ke/main/fetch-ke -o fetch-ke
+curl -L https://raw.githubusercontent.com/MemerGamer/fetch-ke/main/fetch-ke -o fetch-ke 2>/dev/null
 
 # Make it executable
 chmod +x fetch-ke
 
-# Move it to /bin (requires sudo)
-sudo mv fetch-ke /bin/fetch-ke
+isTermux=$(echo $PREFIX | grep -o "com.termux")
+
+# Check if it's Termux
+if [[ -n $isTermux ]]; then
+    # If it's Termux, check if figlet is installed
+    if ! command -v figlet &>/dev/null; then
+        # If not, install it
+        apt install figlet
+    fi
+
+    # Check if ncurses is installed
+    if ! command -v ncurses &>/dev/null; then
+        # If not, install it
+        apt install ncurses
+    fi
+    mv fetch-ke /bin/fetch-ke
+else
+
+    # If it's not Termux
+    # Move it to /bin (requires sudo)
+    echo "please enter your sudo password to install: "
+    sudo mv fetch-ke /bin/fetch-ke
+fi
 
 echo "fetch-ke installed successfully."
